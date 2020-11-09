9bce2273ee3fa98a494bf58ba2d6a7b6d8ac9184
Thiago Teixeira <thiagoemecal@gmail.com>
2020-11-09 09:13:16 -0300

    ts

--- src/views/produtos/cadastro.js
+++ src/views/produtos/cadastro.js
@@ -12,10 +12,10 @@ export default class CadastroProduto extends Component {
             copo é motivis de denguis. Posuere libero varius. Nullam a nisl ut
             ante blandit hendrerit. Aenean sit amet nisi. Delegadis gente finis,
             bibendum egestas augue arcu ut est. Mais vale um bebadis
-            conhecidiss, que um alcoolatra anonimis. Tá deprimidis, eu conheço
-            uma cachacis que pode alegrar sua vidis. Em pé sem cair, deitado sem
-            dormir, sentado sem cochilar e fazendo pose. Casamentiss faiz
-            malandris se pirulitá. Não sou faixa preta cumpadi, sou preto
+            conhecidiss, que um alcoolatra anonimis. <br /> Tá deprimidis, eu
+            conheço uma cachacis que pode alegrar sua vidis. Em pé sem cair,
+            deitado sem dormir, sentado sem cochilar e fazendo pose. Casamentiss
+            faiz malandris se pirulitá. Não sou faixa preta cumpadi, sou preto
             inteiris, inteiris.
           </p>
         </div>
