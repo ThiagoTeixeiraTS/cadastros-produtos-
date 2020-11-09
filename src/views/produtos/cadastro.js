84ca76ec26f239d1c03d2a43ffc3fe39b2e03d40
Thiago Teixeira <thiagoemecal@gmail.com>
2020-11-09 09:07:18 -0300

    criando template, views, rotas

---
+++ src/views/produtos/cadastro.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class CadastroProduto extends Component {
+  render() {
+    return (
+      <div className="card border-secondary mb-3">
+        <div className="card-header">Cadastro de Produtos</div>
+        <div className="card-body">
+          <h4 className="card-title">Categoria</h4>
+          <p className="card-text">
+            Mussum Ipsum, cacilds vidis litro abertis. Diuretics paradis num
+            copo é motivis de denguis. Posuere libero varius. Nullam a nisl ut
+            ante blandit hendrerit. Aenean sit amet nisi. Delegadis gente finis,
+            bibendum egestas augue arcu ut est. Mais vale um bebadis
+            conhecidiss, que um alcoolatra anonimis. Tá deprimidis, eu conheço
+            uma cachacis que pode alegrar sua vidis. Em pé sem cair, deitado sem
+            dormir, sentado sem cochilar e fazendo pose. Casamentiss faiz
+            malandris se pirulitá. Não sou faixa preta cumpadi, sou preto
+            inteiris, inteiris.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
